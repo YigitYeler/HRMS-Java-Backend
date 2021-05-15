399809ad13a40d460ed668b762189d31f8b1ef23
YigitYeler <yeleryigit40@gmail.com>
2021-05-15 18:35:06 +0300

    first commit

---
+++ src/main/java/kodlamaio/hrms/entities/concretes/Position.java
@@ -0,0 +1,41 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "job_positions")
+public class Position {
+	@Id
+	@GeneratedValue
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="position_name")
+	private String positionName;
+
+	public Position() {
+		
+	}
+	
+	public Position(int id, String positionName) {
+		super();
+		this.id = id;
+		this.positionName = positionName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getPositionName() {
+		return positionName;
+	}
+
+	public void setPositionName(String positionName) {
+		this.positionName = positionName;
+	}
+}
